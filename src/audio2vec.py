69295818e90fe472ce919e01c0c2f265c1ed03d0
muoten <muoten@github.com>
2025-01-05 15:14:15 +0100

    add: `audio2vec.py`

---
+++ src/audio2vec.py
@@ -0,0 +1,100 @@
+import chromaprint
+
+from config import *
+import eyed3
+import numpy as np
+import os
+import random
+from chromaprint_utils import refine_vectors_with_best_offsets, get_array_from_fingerprint_encoded
+from src.chromaprint2vec import convert_input_to_vectors_csv
+from src.pychromagram import load_audio_file, get_chromagram
+if USE_FINGERPRINTS:
+    from src.pychromaprint import FingerprinterConfigurationTest2, FingerprintCalculator
+    # Create the configuration
+    config = FingerprinterConfigurationTest2()
+    # Create the FingerprintCalculator using the configuration
+    calculator = FingerprintCalculator(config)
+
+random.seed(RANDOM_SEED)
+
+
+def audiofile_to_chromagram(audiofile):
+    data = load_audio_file(audiofile, convert_to_mono_11025hz=True)
+    chromagram = get_chromagram(data)
+    return chromagram
+
+
+def get_sorted_audio_filenames(folder):
+    sorted_audio_filenames = sorted([f for f in os.listdir(folder)])
+    return sorted_audio_filenames
+
+
+def chromagram_to_chromaprint(chromagram):
+    calculator.consume(chromagram.data)
+    fingerprint = calculator.get_fingerprint()
+
+    decoded_fp_actual = fingerprint
+    encoded_fp_actual = chromaprint.encode_fingerprint(decoded_fp_actual, algorithm=2)
+    return encoded_fp_actual
+
+
+def generate_vectors_from_audio_files(folder):
+    vectors = []
+    sorted_audio_filenames = get_sorted_audio_filenames(folder)
+    for audio_filename in sorted_audio_filenames:
+        chromagram = audiofile_to_chromagram(f"{folder}/{audio_filename}")
+        vector_i_fingerprint = np.array([])
+        vector_i_chromagram = np.array([])
+        if USE_FINGERPRINTS:
+            fingerprint_encoded = chromagram_to_chromaprint(chromagram)
+            print(fingerprint_encoded)
+            array = get_array_from_fingerprint_encoded(fingerprint_encoded, debug=IS_DEBUG)
+            vector_i_fingerprint = array.reshape(-1)
+        if USE_CHROMAGRAMS:
+            vector_i_chromagram = np.array(chromagram.data).reshape(-1)
+        vector_i = np.concatenate([vector_i_chromagram, vector_i_fingerprint])
+        vectors.append(vector_i)
+    max_length = max(len(vector_i) for vector_i in vectors)  # Find the maximum length of vectors
+
+    assert len(vector_i) > 0, f"{audio_filename} gets empty chromagram"
+    # Repeat the content of each vector until it reaches the max_length
+    vectors = [np.tile(vector_i, int(np.ceil(max_length / len(vector_i))))[:max_length] for vector_i in vectors]
+
+    if IS_DEBUG:
+        print(vectors)
+    if FIND_BEST_OFFSET:
+        # truncated vectors to accelerate refine_vectors_with_best_offsets
+        vectors_truncated = [vector[:MINIMAL_LENGTH_VECTORS] for vector in vectors]
+
+        offsets, vectors_refined, adhoc_mapping = refine_vectors_with_best_offsets(vectors_truncated)
+        # regenerate vectors after truncation
+        vectors = [np.concatenate((vector[int(offsets[i]):], vector[:int(offsets[i])])) for i, vector in enumerate(vectors)]
+    return vectors, adhoc_mapping
+
+
+def collect_metadata_from_audio_files():
+    folder = os.path.expanduser(FOLDER_AUDIO_FILES)
+    sorted_audio_filenames = get_sorted_audio_filenames(folder)
+    lines = []
+    for audio_filename in sorted_audio_filenames:
+        audio = eyed3.load(f"{folder}/{audio_filename}")
+
+        # Extract metadata
+        print(f"Title: {audio.tag.title}")
+        print(f"Artist: {audio.tag.artist}")
+        print(f"Album: {audio.tag.album}")
+        print(f"Genre: {audio.tag.genre}")
+        print(f"Duration: {audio.info.time_secs:.2f} seconds")
+        line = f"{audio.tag.artist}\t{audio.tag.title}\t{audio.info.time_secs}"
+        lines.append(line)
+
+    # Split each line into artist and title
+    df = [line.split("\t") for line in lines]
+    return df
+
+
+if __name__ == "__main__":
+    myfolder = os.path.expanduser(FOLDER_AUDIO_FILES)
+    function_to_collect_metadata = collect_metadata_from_audio_files
+    convert_input_to_vectors_csv(generate_vectors_from_audio_files, myfolder, function_to_collect_metadata)
+
